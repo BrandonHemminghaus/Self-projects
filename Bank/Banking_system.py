665218fac118dbe9b45703d7e3fe55e85f92371a
Brandon Hemminghaus <brandonhemminghaus2@gmail.com>
2024-01-09 21:58:24 -0500

    added functions to banking system file

--- Bank/Banking_system.py
+++ Bank/Banking_system.py
@@ -2,6 +2,25 @@
 Created on January 06, 2024
 Author: Brandon Hemminghaus
 '''
+name = ""
+account_number = 0
+balance = 0
+
+def account(n):
+    print(name)
+    print(account_number)
+
+def deposit(amount):
+    balance = balance + amount
+    print(balance)
+
+def withdraw(amount):
+    w = balance - amount
+    if w < 0:
+        return False
+    else:
+        balance = balance - amount
+        return w
 
 active = True
 while(active == True):
@@ -16,8 +35,14 @@ while(active == True):
         print("1" + "\n")
     elif(choice == "2" + "\n"):
         print("2")
+        deposit(50)
     elif(choice == "3" + "\n"):
         print("3")
+        w = withdraw(50)
+        if w == False:
+            print("Not enough money in account")
+        else:
+            print("You have withdrawed " + w + "dollars")
     elif(choice == "4" + "\n"):
         active = False
     else:
