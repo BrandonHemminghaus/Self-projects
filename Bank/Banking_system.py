f75b10d5c759364e8d9748c28c0361314fe92fb5
Brandon Hemminghaus <brandonhemminghaus2@gmail.com>
2024-01-18 22:20:06 -0500

    Add documentation to functions + cleaning lines of code

--- Bank/Banking_system.py
+++ Bank/Banking_system.py
@@ -4,23 +4,41 @@ Author: Brandon Hemminghaus
 '''
 
 class Account_details:
+    """
+    Contains all the values for the user's bank details
+    """
     name = "Brandon Hemminghaus"
     account_number = 16483724
     balance = 0
 
 def account():
+    """
+    Displays all the account detail values at once
+    """
     print("Account name: " + Account_details.name)
     print("Account number: " + str(Account_details.account_number))
     print("Account balance: " + str(Account_details.balance) + "\n")
 
 def deposit(amount):
+    """
+    Adds to the balance value (depositing money)
+
+    :param int amount: the amount of money the user wishes to deposit
+    """
     Account_details.balance += amount
 
 def withdraw(amount):
+    """
+    Subtracts from the balance value (withdrawing money)
+
+    :param int amount: the amount of money the user wishes to withdraw
+    :return -1: if withdraw is bigger than balance
+    :return int amount: returns the amount withdrawed, confirming the process went throught
+    """
     balance = Account_details.balance
-    w = balance - amount
-    if w < 0:
-        return False
+    check = balance - amount
+    if check < 0:
+        return -1
     else:
         Account_details.balance -= amount
         return amount
@@ -32,25 +50,22 @@ while(active == True):
     print("2: Deposit money")
     print("3: Withdraw money")
     print("4: Exit system")
-    choice = input("Please choose an option: ")
-    choice_int = int(choice)
+    choice = int(input("Please choose an option: "))
 
-    if(choice_int == 1):
+    if(choice == 1):
         account()
-    elif(choice_int == 2):
-        d = input("Please input the amount to deposit: ")
-        d_int = int(d)
-        deposit(d_int)
+    elif(choice == 2):
+        d = int(input("Please input the amount to deposit: "))
+        deposit(d)
         print("Current balance: " + str(Account_details.balance))
-    elif(choice_int == 3):
-        w = input("Please input the amount to withdraw: ")
-        w_int = int(w)
-        w_final = withdraw(w_int)
-        if w_final == False:
+    elif(choice == 3):
+        w = int(input("Please input the amount to withdraw: "))
+        w_final = withdraw(w)
+        if w_final == -1:
             print("Not enough money in account")
         else:
             print("You have withdrawed " + str(w) + " dollars")
-    elif(choice_int == 4):
+    elif(choice == 4):
         active = False
     else:
         print("Not a option, pick again" + "\n")
