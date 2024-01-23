9f6443ab48ea46ed4082608ce37256737de0d207
Brandon Hemminghaus <brandonhemminghaus2@gmail.com>
2024-01-22 22:25:50 -0500

    Made better use for class function

--- Pokemon/Battle.py
+++ Pokemon/Battle.py
@@ -3,25 +3,33 @@ Created on January 21, 2024
 Author: Brandon Hemminghaus
 '''
 
-class Trutwig:
-    def __init__(self):
-        self.hp = 200
-        self.defe = 50
-        self.speed = 20
-        self.move_names = ["Razor leaf", "Tackle"]
-        self.move_damage = [50, 30]
+class Pokemon:
+    def __init__(self,name,hp,defe,speed,moves):
+        self.name = name
+        self.hp = hp
+        self.defe = defe
+        self.speed = speed
+        self.moves = moves
     
     def display(self):
-        print("Trutwig stats:")
+        print(self.name + " stats:")
         print("Hp: " + str(self.hp))
         print("Defense: " + str(self.defe))
         print("Speed: " + str(self.speed))
-        print("Moves:")
-        for i in range(len(self.move_names)):
-            print(" Move " + str(i+1) + ": " + self.move_names[i] + "(" + str(self.move_damage[i]) + ")")
+        print("Moves: " + str(self.moves) + "\n")
+    
+    def pick_move(self,m):
+        print(self.name + " used " + m)
+        return self.moves.get(m)
 
 print("Welcome to the pokemon world's final match")
-print("Battle between Shirou and DoubleProtect")
-print("Pick your pokemon")
-mon_1 = Trutwig()
-mon_1.display()
+print("Battle between Shirou_BH and Operation_X")
+print("Pick your pokemon:")
+Turtwig = Pokemon("Turtwig",200,50,20,{"Razor leaf": 40, "Tackle": 30})
+Turtwig.display()
+
+Chimchar = Pokemon("Chimchar",150,25,60,{"Ember": 50, "Scratch": 30})
+Chimchar.display()
+
+Piplup = Pokemon("Piplup",180,40,40,{"Water gun": 40, "Peck": 30})
+Piplup.display()
